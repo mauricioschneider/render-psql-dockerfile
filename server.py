fafc00593821a4419085374dc2aecb2690223c37
Mauricio Schneider <mauricio@render.com>
2025-10-02 14:03:54 -0700

    Add host to python server

--- server.py
+++ server.py
@@ -3,11 +3,12 @@ import http.server
 import socketserver
 
 PORT = 10000
+HOST = "0.0.0.0"
 
 # Use SimpleHTTPRequestHandler for serving files from the current directory
 Handler = http.server.SimpleHTTPRequestHandler
 
-print(f"Serving at port {PORT}")
-# Start the server
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+print(f"Serving at {HOST}:{PORT}")
+# Start the server with the host specified
+with socketserver.TCPServer((HOST, PORT), Handler) as httpd:
     httpd.serve_forever()
